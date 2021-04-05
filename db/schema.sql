565007068ede714d41a310e2c9bf8f66920f05b4
Johan Bejar <johanbejarc@hotmail.com>
2021-04-05 15:04:24 -0400

    Schema designed and created SQL

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,146 @@ CREATE DATABASE runashville;
 
 USE runashville;
 
-CREATE TABLE user (
+CREATE TABLE user_type (
   id INT AUTO_INCREMENT,
-  username VARCHAR(20),
-  name VARCHAR(30),
-  lastName VARCHAR(40),
-  password VARCHAR(100),
-  email VARCHAR(30),
-  created_at DATE,
+  name VARCHAR(20),
+  description TEXT,
   PRIMARY KEY (id)
 );
 
-INSERT INTO user (username, name, lastname, password, email, created_at) VALUES ('Jodi', 'Jodi', 'Jodi', '67eb8695094491a291bfe0d016e639dfc3475c3f80a59931c97246677aaeb711', 'Jodi', '2021-04-03');
+CREATE TABLE user (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30),
+  lastName VARCHAR(50),
+  password VARCHAR(100),
+  email VARCHAR(40),
+  fk_user_type_id INT,
+  banned BOOLEAN,
+  image_url VARCHAR(200),
+  created_at TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_user_type_id) REFERENCES user_type(id)
+);
 
+CREATE TABLE friends (
+  id INT AUTO_INCREMENT,
+  fk_source_user_id INT,
+  fk_target_user_id INT,
+  created_at TIMESTAMP,
+  status BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_source_user_id) REFERENCES user(id),
+  FOREIGN KEY (fk_target_user_id) REFERENCES user(id)
+);
+
+CREATE TABLE post (
+  id INT AUTO_INCREMENT,
+  fk_user_id INT,
+  image_url VARCHAR(200),
+  message TEXT,
+  reported BOOLEAN,
+  created_at TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_user_id) REFERENCES user(id)
+);
+
+CREATE TABLE stats (
+  id INT AUTO_INCREMENT,
+  fk_post_id INT,
+  distance DECIMAL(5,3),
+  time_hour INT,
+  time_minutes INT,
+  time_seconds INT,
+  pace INT,
+  heart_rate INT,
+  steps INT,
+  calories_burned DECIMAL (3,2),
+  created_at TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_post_id) REFERENCES post(id)
+);
+
+CREATE TABLE comments (
+  id INT AUTO_INCREMENT,
+  fk_post_id INT,
+  fk_user_id INT,
+  message TEXT,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_post_id) REFERENCES post(id),
+  FOREIGN KEY (fk_user_id) REFERENCES user(id)
+);
+
+CREATE TABLE reactions (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(20),
+  description TEXT,
+  icon varchar(200),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE reactions_on_post (
+  id INT AUTO_INCREMENT,
+  fk_post_id INT,
+  fk_user_id INT,
+  fk_reaction_id INT,
+  created_at TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_post_id) REFERENCES post(id),
+  FOREIGN KEY (fk_user_id) REFERENCES user(id),
+  FOREIGN KEY (fk_reaction_id) REFERENCES reactions(id)
+);
+
+CREATE TABLE event_type (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(50),
+  description TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE difficulty_level (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30),
+  description TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE events (
+  id INT AUTO_INCREMENT,
+  fk_event_type_id INT,
+  fk_difficulty_level_id INT,
+  fk_leader_user_id INT,
+  official BOOLEAN,
+  start_time DATETIME,
+  end_time DATETIME,
+  start_location VARCHAR(100),
+  end_location VARCHAR(100),
+  image_url VARCHAR(200),
+  thumbnail_photo VARCHAR(200),
+  running_distance DECIMAL(3,2),
+  link VARCHAR(200),
+  attendees INT,
+  promoted BOOLEAN,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_event_type_id) REFERENCES event_type(id),
+  FOREIGN KEY (fk_difficulty_level_id) REFERENCES difficulty_level(id),
+  FOREIGN KEY (fk_leader_user_id) REFERENCES user(id)
+);
+
+CREATE TABLE user_type_for_event_type (
+  id INT AUTO_INCREMENT,
+  fk_event_type_id INT,
+  fk_user_type_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_event_type_id) REFERENCES event_type(id),
+  FOREIGN KEY (fk_user_type_id) REFERENCES user_type(id)
+);
+
+/* INSERT INTO user (username, name, lastname, password, email, created_at) VALUES ('Jodi', 'Jodi', 'Jodi', '67eb8695094491a291bfe0d016e639dfc3475c3f80a59931c97246677aaeb711', 'Jodi', '2021-04-03');
+*/
 /* MOCK USERNAME: Jodi
   MOCK PASSWORD: Jodi
 */
