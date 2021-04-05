52991c7c974e0a0b8b54d77c551c27e1ccbabef1
Daniel Doyle <ddoyle196@gmail.com>
2021-04-05 14:47:25 -0500

    Saving progress before merging navbar

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -4,12 +4,25 @@ const Profile = class extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      userProfile: 'Name',
+      userProfile: {
+        name: 'Test',
+        stats: {
+          avgMilePace: '',
+          recordMilePace: '',
+          lifetimeMiles: '',
+          lifetimeRaces: '',
+          eventsAttended: '',
+        },
+        pic: '',
+        headerImage: '',
+        bio:''
+      },
     };
   }
 
   render() {
     const { userProfile } = this.state;
+    const { name, stats, pic, headerImage } = userProfile;
     return (
       <div>
         <div className="allHeader">
@@ -17,25 +30,25 @@ const Profile = class extends React.PureComponent {
             <div className="headerInfo leftItem">RUNashville</div>
             <div className="headerInfo rightItem">Edit My Page</div>
           </div>
-          <div className="headerImage"></div>
+          <div className="headerImage">{headerImage}</div>
           <div className="profileCard">
-            <div className="profileImage"></div>
-            <div className="userName">{userProfile}</div>
-            <button className="profileButton">Follow {userProfile}</button>
+            <div className="profileImage">{pic}</div>
+            <div className="userName">{name}</div>
+            <button className="profileButton">Follow {name}</button>
           </div>
         </div>
         <div className="profileBody">
           <div className="thirdsColumn">
             <div className="contentBox">
-              <div className="boxTitle">{userProfile}'s Stats</div>
+              <div className="boxTitle">{name}'s Stats</div>
               <div className="statistics"></div>
             </div>
             <div className="contentBox">
-              <div className="boxTitle">{userProfile}'s Events</div>
+              <div className="boxTitle">{name}'s Events</div>
               <div className="eventsFeed"></div>
             </div>
             <div className="contentBox">
-              <div className="boxTitle">{userProfile}'s Recent Activities</div>
+              <div className="boxTitle">{name}'s Recent Activities</div>
               <div className="recentActivityFeed"></div>
             </div>
           </div>
@@ -43,18 +56,22 @@ const Profile = class extends React.PureComponent {
             <button className="profileButton">+ New Post</button>
             <button className="profileButton">+ Create Daily Run</button>
             <div className="contentBox">
-              <div className="boxTitle">{userProfile}'s Feed</div>
+              <div className="boxTitle">{name}'s Feed</div>
               <div className="profileFeed"></div>
             </div>
           </div>
           <div className="thirdsColumn">
             <div className="contentBox">
-              <div className="boxTitle">{userProfile}'s Bio</div>
+              <div className="boxTitle">{name}'s Bio</div>
               <div className="userBio"></div>
             </div>
             <div className="contentBox">
-              <div className="boxTitle">{userProfile}'s Friends</div>
+              <div className="boxTitle">{name}'s Friends</div>
               <div className="friendsList"></div>
+            </div>
+            <div className="contentBox">
+              <div className="boxTitle">{name}'s Friends Updates</div>
+              <div className="friendFeed"></div>
             </div>
           </div>
         </div>
