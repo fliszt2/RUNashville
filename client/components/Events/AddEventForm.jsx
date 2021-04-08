97d3efadebb9765427f1ec1e70f6c81f75b4c18f
fliszt2 <rkelly2012@gmail.com>
2021-04-08 16:18:13 -0400

    cleaning up add event form

--- client/components/Events/AddEventForm.jsx
+++ client/components/Events/AddEventForm.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import Modal from 'react-bootstrap/Modal';
+// import Modal from 'react-bootstrap/Modal';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 class AddEventForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      event_name: 'Text goes here...',
-      event_type: 'Select',
-      link: 'http://www.example-url.com',
+      event_name: '',
+      event_type: '',
+      link: '',
       start_time: 'MMDDYYYY',
+      start_date: new Date(),
       start_location: '',
       map_url: '',
       description: '',
       image_url: '',
-      running_distance: null,
-      difficulty_level: '',
+      running_distance: 0,
+      difficulty_level: 'beginner',
 
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
   }
 
   handleInputChange(event) {
@@ -32,29 +37,67 @@ class AddEventForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // var body = {
-    //   "formName1": this.state.formName1,
-    //   "formName2": this.state.formName2
-    // };
-    // return axios.post('/endpoint', body)
-    //   .then(() => {
-    //     alert('Record has been Added!');
-    //   })
-    //   .then(() => {
-    //     this.setState({
-    //       formName1: '',
-    //       formName2: ''
-    //     })
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   })
+    // if any field is empty, alert
+    const { event_name, event_type, link, start_time, start_date, start_location, map_url, description, image_url, running_distance, difficulty_level } = this.state;
+    start_date = start_date.toUTCString();
+    console.log('start_date:', start_date);
+    const stateValues = [event_name, event_type, link, start_time, start_date, start_location, map_url, description, image_url, running_distance, difficulty_level];
+
+    for (let value of stateValues) {
+      if (value === null) {
+        value = '';
+      }
+      let valueAsString = value.toString();
+      if (valueAsString.length === 0 || valueAsString === 'Select') {
+        return alert('Please fill in all fields.');
+      }
+    }
+
+    const data = {
+      event_name: event_name,
+      event_type: event_type,
+      link: link,
+      start_time: start_time,
+      start_date: start_date,
+      start_location: start_location,
+      map_url: map_url,
+      description: description,
+      image_url: image_url,
+      running_distance: running_distance,
+      difficulty_level: difficulty_level,
+    };
+
+    axios.post('/events', data)
+      .then(() => {
+        console.log('submitted');
+        this.setState({
+          event_name: '',
+          event_type: '',
+          link: '',
+          start_time: 'MMDDYYYY',
+          start_date: new Date(),
+          start_location: '',
+          map_url: '',
+          description: '',
+          image_url: '',
+          running_distance: null,
+          difficulty_level: 'beginner',
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   createMap(e) {
     e.preventDefault();
     const url = 'https://onthegomap.com/#/create';
     window.open(url, '_blank');
+  }
+  handleDateChange(date) {
+    this.setState({
+      start_date: date
+    })
   }
 
   render() {
@@ -67,12 +110,13 @@ class AddEventForm extends React.Component {
           <br></br>
           <form>
             <label>Event Name:
-              <input name="event_name" type="text" value={event_name} onChange={this.handleInputChange} />
+              <input name="event_name" type="text" placeholder="Text goes here..." value={event_name} onChange={this.handleInputChange} />
             </label>
             <br />
             <label>
               Type of Event:
               <select name="event_type" value={event_type} onChange={this.handleInputChange}>
+                <option value="Select">Select</option>
                 <option value="Race">Race</option>
                 <option value="Daily Group Run">Daily Group Run</option>
                 <option value="Announcement">Announcement</option>
@@ -81,12 +125,24 @@ class AddEventForm extends React.Component {
             <br />
             <label>
               Registration Page:
-              <input name="link" type="text" value={link} onChange={this.handleInputChange} />
+              <input name="link" type="text" placeholder="http://www.example-url.com" value={link} onChange={this.handleInputChange} />
             </label>
             <br />
             <label>
               Event Time:
-              <input name="start_time" type="text" value={start_time} onChange={this.handleInputChange} />
+              {/* <input name="start_time" type="text" value={start_time} onChange={this.handleInputChange} /> */}
+              <input type="time"  name="start_time" min="00:00" max="23:55" onChange={this.handleInputChange}></input>
+            </label>
+            <br />
+            <label>
+              Date:
+              {/* <input name="start_time" type="text" placeholer="MMDDYYY" value={start_time} onChange={this.handleInputChange} /> */}
+              <DatePicker
+              selected={ this.state.start_date }
+              onChange={ this.handleDateChange }
+              name="start_date"
+              dateFormat="MM/dd/yyyy"
+          />
             </label>
             <br />
             <label>
