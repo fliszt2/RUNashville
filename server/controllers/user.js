014d737b58e8aff6a549ea90b82555016fe8b4c9
Johan Bejar <johanbejarc@hotmail.com>
2021-04-08 15:03:13 -0400

    User Update power route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -54,5 +54,61 @@ module.exports = {
         userId: id,
       }
     */
+    const { update, value, userId } = req.params;
+    const data = [value, userId];
+    if (update === 'banned') {
+      models.user.putUpdateUserBanned(data, (result) => {
+        if (result.length !== 0) {
+          res.status(204).json(result);
+        } else {
+          res.status(404).send('User Not Found');
+        }
+      });
+    }
+    if (update === 'name') {
+      models.user.putUpdateUserName(data, (result) => {
+        if (result.length !== 0) {
+          res.status(204).json(result);
+        } else {
+          res.status(404).send('User Not Found');
+        }
+      });
+    }
+    if (update === 'profile_picture') {
+      models.user.putUpdateUserProfilePicture(data, (result) => {
+        if (result.length !== 0) {
+          res.status(204).json(result);
+        } else {
+          res.status(404).send('User Not Found');
+        }
+      });
+    }
+    if (update === 'profile_banner') {
+      models.user.putUpdateUserBannerPicture(data, (result) => {
+        if (result.length !== 0) {
+          res.status(204).json(result);
+        } else {
+          res.status(404).send('User Not Found');
+        }
+      });
+    }
+    if (update === 'last_name') {
+      models.user.putUpdateUserLastName(data, (result) => {
+        if (result.length !== 0) {
+          res.status(204).json(result);
+        } else {
+          res.status(404).send('User Not Found');
+        }
+      });
+    }
+    if (update === 'address_user') {
+      models.user.putUpdateUserAddress(data, (result) => {
+        if (result.length !== 0) {
+          res.status(204).json(result);
+        } else {
+          res.status(404).send('User Not Found');
+        }
+      });
+    }
   },
 };
