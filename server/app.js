565f3cec11f2b1b88a4ba5d6428e2b93bcb27bd7
fliszt2 <rkelly2012@gmail.com>
2021-04-09 20:02:14 -0400

    fixed insertions again

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const router = require('./routes');
-// const axios = require('axios');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
